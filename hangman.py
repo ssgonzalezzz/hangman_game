a51977f408f6db137d5916f584eafd017010f8a7
Samuel <samgon0909@gmail.com>
2022-03-28 22:11:40 -0400

    Added error handling. Next step: Lives system (dont know how iam gonna do it)

--- hangman.py
+++ hangman.py
@@ -3,24 +3,30 @@ from os import  system
 
 def run():
     FILE_RUTE = 'files/data.txt'
-
     word_list = read_file(FILE_RUTE)
     word = normalize_word(choose_random_word(word_list))
     system('clear')
     guess = (get_initial_string(word))
+
     while True:
-        if guess == list(word):
-            break
-        system('clear')
-        print_guess(guess)
-        print('\n')
-        # print(word)
-        letra = input('Intenta con una nueva letra: ').upper()
-        for indx, char in enumerate(word):
-            # print(indx, char)
-            if letra == char:
-                guess[indx] = letra
-    
+        try:
+            if guess == list(word):
+                break
+            system('clear')
+            print_guess(guess)
+            print('\n')
+            # print(word)
+            letra = input('Intenta con una nueva letra: ').upper()
+            if len(letra) != 1:
+                raise Exception('El argumento introducido debe ser una letra')
+            for indx, char in enumerate(word):
+                # print(indx, char)
+                if letra == char:
+                    guess[indx] = letra
+        except Exception as ex:
+            print(' ')
+            print(ex)
+            input('Presiona cualquier tecla para continuar')
 
     system('clear')
     print(f'Ganaste! La palabra que lograste adivinar es {word}')
